33a268107774d41ecf65d65d0bfa3266b983489c
hisham-css <hisham.ata@hotmail.com>
2021-01-29 11:11:01 -0500

    Lab 2 - unfinished

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    public float speed;
+    public int jumpForce;
+    public bool isGrounded;
+    public LayerMask isGroundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 100;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.01f;
+        }
+
+        if (!groundCheck)
+        {
+            Debug.Log("Groundcheck does not exist, please set a transform value for groundcheck");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        anim.SetFloat("speed", Mathf.Abs(horizontalInput));
+        anim.SetBool("isGrounded", isGrounded);
+    }
+}
